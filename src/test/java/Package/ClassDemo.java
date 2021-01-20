8b100bcdb909d4d22dbd36442ee4b711d2969784
ZehraDede <64098150+ZehraDede@users.noreply.github.com>
2021-01-20 18:10:29 -0500

    last update from remote master branch

--- src/test/java/Package/ClassDemo.java
+++ src/test/java/Package/ClassDemo.java
@@ -4,6 +4,7 @@ public class ClassDemo {
     public static void main(String[] args) {
         //Hello everyone
         System.out.println("Here are my updates about DemoClass");
+        System.out.println("A colaborator updated");
     }
 
     //Hello everybody
