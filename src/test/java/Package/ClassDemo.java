654d1845773b3f98094da578560b138ccee91d57
Zehra Dede <you@example.com>
2021-01-20 17:46:07 -0500

    some updates from local zehra

--- src/test/java/Package/ClassDemo.java
+++ src/test/java/Package/ClassDemo.java
@@ -2,7 +2,8 @@ package Package;
 
 public class ClassDemo {
     public static void main(String[] args) {
-        //Hello everyone 
+        //Hello everyone
+        System.out.println("Here are my updates about DemoClass");
     }
 
     //Hello everybody
